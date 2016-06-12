61f0d7f319642f1ab205bbf85108a84e50891ee1
lingtran <lxiutran@gmail.com>
2016-06-12 02:39:11 -0600

    Pass tests for calculating weighted score for both user and user deck across users

--- app/models/user_deck.rb
+++ app/models/user_deck.rb
@@ -4,8 +4,11 @@ class UserDeck < ActiveRecord::Base
   belongs_to :deck
   belongs_to :user
 
-
   def weighted_score_over_time
     study_sessions.sum("score * weight")/study_sessions.sum("weight")
+  end
+
+  def self.weighted_score_across_users_over_time
+    joins(:study_sessions).sum("score * weight")/joins(:study_sessions).sum("weight")
   end
 end
