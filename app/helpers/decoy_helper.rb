a229b7dca97f6a8b9ae75ce3ddddb3912cf1afcd
lingtran <lxiutran@gmail.com>
2016-06-14 08:19:07 -0600

    add to decoy helper to pull logic from view

--- app/helpers/decoy_helper.rb
+++ app/helpers/decoy_helper.rb
@@ -1,5 +1,13 @@
 module DecoyHelper
-  def decoys(quizzed_word)
-    WrongChoices.options_for(quizzed_word)
+  def maybe_decoy_for(deck_word)
+    deck_word.pinyin
+  end
+  
+  def decoy_for(quizzed_word)
+    WrongChoices.options_for(quizzed_word).first.pinyin
+  end
+
+  def another_decoy_for(quizzed_word)
+    WrongChoices.options_for(quizzed_word).last.pinyin
   end
 end
