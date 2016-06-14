c9163f1d6259dea0df10f4049b5b855238c35497
lingtran <lxiutran@gmail.com>
2016-06-14 07:59:01 -0600

    refactor flashcards card partial

--- app/controllers/study_sessions_controller.rb
+++ app/controllers/study_sessions_controller.rb
@@ -1,27 +1,29 @@
 class StudySessionsController < FlashcardsController
-  before_filter :find_deck_word, only: [:create]
+  before_filter :find_deck_word, :first_deck_word?, :last_deck_word?, :answer_correct?, only: [:create]
 
   helper_method :deck_word,
-                :last_deck_word?,
-                :first_deck_word?,
                 :next_word,
-                :answer_correct?
+                :next_page
 
   def create
     if first_deck_word?
       session[:tallied_score] = 0
       session[:tallied_score] += 1 if answer_correct?
-      redirect_to flashcard_page_user_path(current_user, params[:deck_id], page: current_deck.index(next_word)+1)
+
+      redirect_to flashcard_page_user_path(current_user, params[:deck_id], page: next_page)
     elsif last_deck_word?
       session[:tallied_score] += 1 if answer_correct?
+
       Score.record(deck_word.deck, current_user, session[:tallied_score])
+
       redirect_to flashcard_user_path(current_user, params[:deck_id])
 
       flash[:score_popup] = "Your score is #{session[:tallied_score]}"
       flash[:encouragement] = "Have another go at it"
     else
       session[:tallied_score] += 1 if answer_correct?
-      redirect_to flashcard_page_user_path(current_user, params[:deck_id], page: current_deck.index(next_word)+1)
+
+      redirect_to flashcard_page_user_path(current_user, params[:deck_id], page: next_page)
     end
   end
 
@@ -38,20 +40,8 @@ class StudySessionsController < FlashcardsController
       deck_word == current_deck.first
     end
 
-    def set_to_zero_if_first_deck_word
-      session[:tallied_score] = 0
-    end
-
-    def score_if_answer_correct?
-      if params[:choice] == deck_word.word.pinyin
-        session[:tallied_score] += 1
-      else
-        session[:tallied_score]
-      end
-    end
-
     def answer_correct?
-      params[:choice] == deck_word.word.pinyin
+      params[:choice] == deck_word.pinyin
     end
 
     def last_deck_word?
@@ -60,5 +50,9 @@ class StudySessionsController < FlashcardsController
 
     def next_word
       DeckQuestionWord.find_next_for(current_deck, deck_word)
+    end
+
+    def next_page
+      current_deck.index(next_word)+1
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  # has_secure_password
-
   has_many :user_decks
   has_many :decks, through: :user_decks
 
@@ -22,6 +20,3 @@ class User < ActiveRecord::Base
     user
   end
 end
-
-# validates :email, presence: true, uniqueness: true
-# validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
